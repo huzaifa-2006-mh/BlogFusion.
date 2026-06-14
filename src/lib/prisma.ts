99f955d53bb6e7b83d901f716625e4a052a68bbf
MUHAMMAD Huzaifa <mhsoftwarehouse77@gmail.com>
2026-06-14 08:30:54 +0500

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,17 @@
-import prisma from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+// Global type safe declaration
+declare const globalThis: {
+  prisma: ReturnType<typeof prismaClientSingleton> | undefined;
+} & typeof global;
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+// Yahan variable ka naam 'prismaClient' rakha hai taaki naam ka koi clash na ho
+const prismaClient = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma;
+export default prismaClient;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismaClient;
