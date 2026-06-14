a350f1098a05fab73dbb37ed45df9a8eec1b6281
MUHAMMAD Huzaifa <mhsoftwarehouse77@gmail.com>
2026-06-14 11:09:56 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,7 +91,7 @@ export default function RootLayout({
           `}
         </Script>
         <script src="https://5gvci.com/act/files/tag.min.js?z=11145911" data-cfasync="false" async></script>
-        <script>(function(s){s.dataset.zone='11145909',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
+        
         <AnalyticsTracker />
         <Navbar />
         <main>{children}</main>
