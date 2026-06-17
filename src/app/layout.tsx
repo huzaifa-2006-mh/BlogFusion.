d0f4ded94b03eaf56ebeb006ee1ade3f47d55630
MUHAMMAD Huzaifa <mhsoftwarehouse77@gmail.com>
2026-06-17 08:59:42 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,7 @@ export default function RootLayout({
     })(document.createElement('script'));
   `}
 </Script>
+        <script>(function(s){s.dataset.zone='11156399',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
         <AnalyticsTracker />
         <Navbar />
         <main>{children}</main>
